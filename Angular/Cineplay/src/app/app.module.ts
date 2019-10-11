8b7841bd65ea4e408c2dba99b0ade236410e1a2d
athavy92 <athavy92@gmail.com>
2019-10-10 23:18:22 -0400

    Components

--- Angular/Cineplay/src/app/app.module.ts
+++ Angular/Cineplay/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
+import { LogoutComponent } from './logout/logout.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    ChatroomComponent,
+    FooterComponent,
+    LoginSignupComponent,
+    LogoutComponent,
+    MainComponent,
+    NavbarComponent,
+    WatchlistComponent
   ],
   imports: [
     BrowserModule,
